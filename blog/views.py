618803316105a7b5d0a4336e5aeca58981d8558e
kiranchoppa <kiran.choppa@beehyv.com>
2024-07-11 17:26:23 +0530

    a stable place

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, "blog/post_list.html", {})
+    me = User.objects.get(username="kiran")
+    posts = Post.objects.filter(author=me)
+    return render(request, "blog/post_list.html", {"posts": posts})
