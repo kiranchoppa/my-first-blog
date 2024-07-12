2fb56124d40a3781112d155b942fb4c85e223ca8
kiranchoppa <kiran.choppa@beehyv.com>
2024-07-12 15:03:55 +0530

    added some objects in shopping items

--- shopping/models.py
+++ shopping/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
 class Items(models.Model):
     name = models.CharField(max_length=20)
     price = models.IntegerField()
-    discount = models.IntegerField(max_length=100)
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+
+    sizes = (
+        ("XS", "Extra Small"),
+        ("S", "Small"),
+        ("M", "Medium"),
+        ("L", "Large"),
+        ("XL", "Extra Large"),
+    )
+    size = models.CharField(max_length=2, choices=sizes)
 
     def __str__(self):
         return self.name
